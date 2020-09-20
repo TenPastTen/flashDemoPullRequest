ea820e976f255cb9e467a53e13e14d25f093b945
Willy Chen <qwe020488952@gmail.com>
2020-09-20 21:14:17 +0800

    commit trash

--- demoPullReauest/src/demoPullReauest/openLucky.java
+++ demoPullReauest/src/demoPullReauest/openLucky.java
@@ -5,6 +5,7 @@ public class openLucky {
 	public static void main(String[] args) {
 		System.out.println("Pull Request 開張大吉!");
 		System.out.println("我這邊commit,你那邊應該會收到?");
+		System.out.println("再度亂搞");
 	}
 
 }
